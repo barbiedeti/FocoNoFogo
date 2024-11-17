deb063bfbbffa54a103e3baa0e9725d8479654c3
Gabrielly "Kim"  Gomes <kimalvesg@outlook.com>
2024-11-17 13:46:41 -0300

    fastapi-backend
    Atualização de API da FIRMS-NASA
    Adicionado redirecionamento de chamada através do Twilio
    Adição de servidor remoto NGROK para redirecionamento das chamadas

--- fastapi.py
+++ fastapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from typing import Optional, List
 import os
@@ -6,7 +6,9 @@ from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse
 import requests
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -16,9 +18,10 @@ app.add_middleware(
     allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.).
     allow_headers=["*"],  # Permite todos os headers.
 )
+
 @app.get("/favicon.ico")
 async def favicon():
-    return FileResponse("static/favicon.ico")
+    return FileResponse("appfogo/appfogo/favicon.ico")
                         
 # Define the location model
 class Location(BaseModel):
@@ -53,7 +56,7 @@ email_bombeiros = {
     "GO": "cbm@go.gov.br",
     "MA": "cbm@ma.gov.br",
     "MG": "cbm@mg.gov.br",
-    "MS": "cbm@ms.gov.br",
+    "MS": "cbm@ms.gov.br", #e-mails ficticios
     "MT": "cbm@mt.gov.br",
     "PA": "cbm@pa.gov.br",
     "PB": "cbm@pb.gov.br",
@@ -73,39 +76,48 @@ email_bombeiros = {
 #Lista para pontos de incêndio
 fire_points = []
 
-# Função para receber ligação
-def send_call(content: str):
-    account_sid = os.environ.get('your_acc_sid')
-    auth_token = os.environ.get('your_auth_token')
-    client = Client(account_sid, auth_token)
 
-    call = client.calls.create(
-       twiml=f'<Response><Say>{content}</Say></Response>',
-        from_='+14783313332',
-        to='+55193'  # Número de emergência
-    )
-
-    print(call.sid)
 # Função de email
-def send_email(subject: str, text: str, to_email: str):
+def send_email(subject: str, content: str, to_email: str):
     api_key = os.environ.get('your_api_key')
     domain = os.environ.get('your_domain')
+    if not api_key or not domain:
+        raise HTTPException(status_code=500, detail="Configurações Inválidas")
     return requests.post(
-        f"https://api.mailgun.net/v3/{domain}/messages",
+        f"https://api.mailgun.net/v3/{domain}mailgun.org/messages",
         auth=("api", api_key),
-        data={"from": "email_exemplo@example.com",
+        data={"from": "<your_domain_mail>",
                 "to": [to_email],
                 "subject": subject,
-                "text": text}
+                "text": content}
                 )
 
 # Endpoint to report a fire
-@app.post("/report_fire")
-async def report_fire(report: FireReport):
+#@app.post("/report_fire") 
+#async def report_fire(report: FireReport):
     # Send the location to the firefighters
-    content = f"Incêndio relatado em {report.location.latitude}, {report.location.longitude}\nDescription: {report.description}"
-    send_call(content)
-    return {"message": "Fogo relatado com sucesso."}
+ #   content = f"Incêndio relatado em {report.location.latitude}, {report.location.longitude}\nDescription: {report.description}"
+  #  send_call(content)
+   # return {"message": "Fogo relatado com sucesso."}
+
+@app.post("/twilio_redirect") #endpoint para redirecionar chamadas
+async def twilio_redirect():
+    account_sid = os.environ.get('your_sid')
+    auth_token = os.environ.get('your_auth')
+    client = Client(account_sid, auth_token)
+
+    call = client.calls.create(
+        url = "https://your_remote_servidor.ngrok/twilio_redirect",
+       #twiml=f'<Response><Say>Por favor tome medidas para se proteger enquanto nossa unidade chega.</Say></Response>',
+        from_='phone_number_from_twilio',
+        to='+55193'  # Número de emergência
+    )
+    print(f"Call SID: {call.sid}")
+    #Encaminhamento
+    response = VoiceResponse()
+    response.say("Ligação encaminhada para o corpo de bombeiros")
+    response.dial("+55193")  # Número de emergência
+    return Response (content=str(response), media_type="application/xml")
 
 # Endpoint to make an anonymous report
 @app.post("/anonymous_report")
@@ -117,14 +129,16 @@ async def anonymous_report(report: AnonymousReport):
    #envia email para o corpo de bombeiros
    subject = "Relatório Anônimo de Incêndio"
    to_email = email_bombeiros[report.state]
-   text = text
-   send_email(subject,text,to_email)
-   return
+   content = f"Descrição: {report.description}"
+   response = send_email(subject,content,to_email)
+   if response.status_code != 200:
+       raise HTTPException(status_code=500, detail="Erro ao enviar email")
+   return {"message": "Denúncia enviada com sucesso."}
 
 #endpoint para pontos de incêndio
 @app.get("/fire_points", response_model=List[FirePoint])
 async def get_fire_point():
-    url = "https://firms.modaps.eosdis.nasa.gov/map/#d:24hrs;@-48.5,-6.1,4.6z"
+    url = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/bd0075c061f10c45f6abdf12854dfbbe/VIIRS_SNPP_NRT/-85,-57,-32,14/1/2024-11-17"
     response = requests.get(url)
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Erro ao obter dados")
@@ -133,16 +147,22 @@ async def get_fire_point():
     lines = response.text.splitlines()
     for line in lines[1:]: #ignora cabeçalho
         parts = line.split(',')
-    try:
-        latitude = float(parts[0])
-        longitude = float(parts[1])
-        fire_points.append(FirePoint(latitude=latitude, longitude=longitude))
-    except (ValueError, IndexError):
-        pass
+        if len(parts) >=2:
+            try:
+                latitude = float(parts[0])
+                longitude = float(parts[1])
+                fire_points.append(FirePoint(latitude=latitude, longitude=longitude))
+            except (ValueError, IndexError):
+                pass
 
     return fire_points
 
 # Error handling
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
-    return {"error": exc.detail}
+   return JSONResponse(
+       status_code=exc.status_code,
+       content={"message": exc.detail}
+   )
+
+
